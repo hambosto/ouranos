f2bf90085ff4dce5bbcec8fece9ba4d90700e35f
Ilham Putra Husada <106536348+hambosto@users.noreply.github.com>
2026-09-12 11:31:27 +0700

    build(nix): remove rust-overlay dependency

--- flake.nix
+++ flake.nix
@@ -4,17 +4,12 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay/stable";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs =
     {
       self,
       nixpkgs,
-      rust-overlay,
       flake-utils,
       ...
     }:
@@ -23,12 +18,6 @@
       let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ rust-overlay.overlays.default ];
-        };
-        toolchain = pkgs.rust-bin.stable.latest.default;
-        rustPlatform = pkgs.makeRustPlatform {
-          cargo = toolchain;
-          rustc = toolchain;
         };
         fmtDate =
           raw:
@@ -48,7 +37,6 @@
             inherit
               date
               rev
-              rustPlatform
               version
               ;
           };
@@ -63,7 +51,7 @@
       }
     )
     // {
-      overlays.default = _: prev: {
+      overlays.default = final: prev: {
         inherit (self.packages.${prev.stdenv.system}) ouranos;
       };
 
